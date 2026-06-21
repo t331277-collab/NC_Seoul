4c7bfc8f4583b3b28b9c750d77e70ce5c1811928
t331277 <t331277@gmail.com>
2026-06-22 00:32:42 +0900

    지형

--- Assets/Scripts/Camera/TerrainClickButton.cs
+++ Assets/Scripts/Camera/TerrainClickButton.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TerrainClickButton : MonoBehaviour
 {
@@ -6,11 +8,36 @@ public class TerrainClickButton : MonoBehaviour
     [SerializeField] private Transform regionTransform;
     [SerializeField] private string regionName;
 
+    private static readonly List<RaycastResult> uiRaycastResults = new List<RaycastResult>();
+
     void OnMouseDown()
     {
+        if (IsPointerOverUi())
+        {
+            return;
+        }
+
         if (uiManager != null)
         {
             uiManager.ShowTerrainPanel(regionName, regionTransform);
         }
     }
+
+    private bool IsPointerOverUi()
+    {
+        if (EventSystem.current == null)
+        {
+            return false;
+        }
+
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        uiRaycastResults.Clear();
+        EventSystem.current.RaycastAll(pointerEventData, uiRaycastResults);
+        return uiRaycastResults.Count > 0;
+    }
+
 }
